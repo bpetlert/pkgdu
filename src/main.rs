e5f976d35401f9be92da8e1dbaa78191bbb16d53
Bhanupong Petchlert <bpetlert@gmail.com>
2022-12-02 06:46:59 +0700

    feat: Use logging instead of default eprintln for main()

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{
     cmp::Reverse,
     io::{self, Write},
+    process::ExitCode,
 };
 
 use alpm::Alpm;
@@ -12,7 +13,7 @@ use tabled::{
     object::{Columns, Rows},
     Alignment, Disable, Modify, Style, Table, Tabled,
 };
-use tracing::debug;
+use tracing::{debug, error};
 use tracing_subscriber::EnvFilter;
 
 #[derive(Debug, Parser)]
@@ -132,7 +133,7 @@ impl std::fmt::Display for FileSize {
     }
 }
 
-fn main() -> Result<()> {
+fn run() -> Result<()> {
     let filter = EnvFilter::try_from_default_env().unwrap_or(EnvFilter::try_new("pkgdu=warn")?);
     tracing_subscriber::fmt()
         .with_env_filter(filter)
@@ -146,10 +147,20 @@ fn main() -> Result<()> {
     debug!("Run with {:?}", arguments);
 
     let mut report = Report::new(arguments.sort, arguments.show_description);
-    report.build()?;
+    report
+        .build()
+        .context("Failed to calculate sum of file sizes for each installed packages")?;
 
     let mut stdout = io::BufWriter::new(io::stdout().lock());
-    writeln!(stdout, "{report}")?;
+    writeln!(stdout, "{report}").context("Could not write report to STDOUT")?;
 
     Ok(())
+}
+
+fn main() -> ExitCode {
+    if let Err(err) = run() {
+        error!("{err:#}");
+        return ExitCode::FAILURE;
+    }
+    ExitCode::SUCCESS
 }
